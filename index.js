7e82488a4363f65cc29dda3e756e6c531beccc64
flor <florenciascarabotti@gmail.com>
2022-10-21 22:57:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+class Animal{
+    constructor(nombre, dueña,raza,edad,actividades,comida, veterinario, vacunas) {
+        this.nombre = nombre; // va asignación porque es una variable
+        this.dueña  = dueña;
+        this.raza   = raza;
+        this.edad   = edad;
+        this.actividades = actividades;
+        this.comida = comida;
+        this.veterinario = veterinario;
+        this.vacunas = vacunas;
+        
+    }
+ 
+}
+let gato = new Animal("China","Gise","Común Europeo","4 años","Dormir y controlar el barrio por la ventana","Alimento balanceado y cada tanto pollo o atun","Cada 4 meses","Completas");
+console.log(gato);
+//inner.html inyecta info en la etiqueta seleccionada
+document.querySelector("li").innerHTML = `Nombre: ${gato.nombre} </br> Dueña: ${gato.dueña} </br> Raza: ${gato.raza} </br> Edad: ${gato.edad} </br> Actividades: ${gato.actividades} </br> Comida: ${gato.comida} </br> Visita al veterinario: ${gato.veterinario} </br> Vacunas: ${gato.vacunas}`;
